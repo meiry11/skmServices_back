c8229fe7cc5914c206b89e169ab2cda9fd280e37
meiry11 <meirycisse11@gmail.com>
2022-08-25 17:43:25 +0200

    first commit

---
+++ src/main/java/com/skmServices/skmServices/SkmServicesApplication.java
@@ -0,0 +1,13 @@
+package com.skmServices.skmServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkmServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkmServicesApplication.class, args);
+	}
+
+}
